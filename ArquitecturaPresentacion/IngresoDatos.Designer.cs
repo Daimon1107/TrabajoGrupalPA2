b7dd451aebef9e41dd89a1bafaa3447dec785415
Damian <damianalban0711@hotmail.com>
2023-12-20 02:02:32 -0500

    lectura de archivo de word

--- ArquitecturaPresentacion/IngresoDatos.Designer.cs
+++ ArquitecturaPresentacion/IngresoDatos.Designer.cs
@@ -32,6 +32,8 @@
             this.button1 = new System.Windows.Forms.Button();
             this.axWebBrowser1 = new AxSHDocVw.AxWebBrowser();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.axWebBrowser1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -43,26 +45,47 @@
             this.button1.TabIndex = 0;
             this.button1.Text = "documento";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.Click += new System.EventHandler(this.button1_Click_2);
             // 
             // axWebBrowser1
             // 
             this.axWebBrowser1.Enabled = true;
-            this.axWebBrowser1.Location = new System.Drawing.Point(631, 12);
+            this.axWebBrowser1.Location = new System.Drawing.Point(592, 12);
             this.axWebBrowser1.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axWebBrowser1.OcxState")));
             this.axWebBrowser1.Size = new System.Drawing.Size(641, 481);
             this.axWebBrowser1.TabIndex = 1;
-            this.axWebBrowser1.Enter += new System.EventHandler(this.axWebBrowser1_Enter);
+            this.axWebBrowser1.DownloadComplete += new System.EventHandler(this.axWebBrowser1_DownloadComplete);
             // 
             // openFileDialog1
             // 
             this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(379, 142);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(148, 52);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(379, 257);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(148, 52);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // IngresoDatos
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1396, 527);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.axWebBrowser1);
             this.Controls.Add(this.button1);
             this.Name = "IngresoDatos";
@@ -78,5 +101,7 @@
         private System.Windows.Forms.Button button1;
         private AxSHDocVw.AxWebBrowser axWebBrowser1;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
